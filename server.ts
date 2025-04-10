551a89ad1fc2f94e48cd1298b597c46fb0380adf
Unique Shrestha <uniquestha619@gmail.com>
2025-04-10 13:04:24 +0545

    done with add todos

--- server.ts
+++ server.ts
@@ -5,21 +5,26 @@ import { Server } from 'socket.io';
 const port = config.port;
 
 
-
 let io: Server | undefined;
 function startServer(){
     const server = app.listen(port,()=>{
         connectDb();
         console.log(`server is started at port ${port}`);
     })
-     io = new Server(server);
+    
+    io = new Server(server);
+    //reason in tsissue.txt file
+    import('../todo/todo/todoController').then(() => {
+        console.log('TodoController loaded after Socket.IO was initialized');
+      });
   
 }
-function returnSocketIo(){
-    if(!io){
-        throw new Error('there is no io');
+function returnSocketIo() {
+    if (!io) {
+        throw new Error('Socket.io instance has not been initialized yet.');
     }
-    return io
+    return io;
 }
+
 startServer();
 export {returnSocketIo};
--- todo/todoController.ts
+++ todo/todoController.ts
@@ -1,6 +1,59 @@
+import { Socket } from "socket.io";
 import { returnSocketIo } from "../server";
+import todoModel from "./todoModel";
+//use oop instead of modular approach
 
-const io = returnSocketIo;
+class Todo{
+    private io = returnSocketIo();
+    //io is private because only one instance of io should be used through out the project and it should not be accessed other than this class
+    constructor(){
+        this.io.on("connection",(socket)=>{
+            console.log(`a client has been connected-${socket.id}`);
+            
+            socket.on('addData',(data)=>{
+                this.addTodo(socket,data);
+            })
+
+        });
+        
+    
+    }
+   //to handle add or remove we will have to define each methods and then bind them 
+   private async addTodo(socket:Socket,data:any){
+    // console.log(`data has been received - ${data.task}`);
+    try {
+        const {task,deadline,status}= data;
+        await todoModel.create({
+           task,
+           deadline,
+           status
+       });
+       const allTodos = await todoModel.find();
+       socket.emit('responseToAddData',{data: allTodos});
+    } catch (error) {
+        console.log(`error in adding todos- ${error}`)
+    }
+    // so instead of sending individual responses using emit we can render all todos and send it as response to optimize the code
+    // if(addData){
+    //     socket.emit('responseToAddData',{
+    //         message:"successfully added data"
+    //     });
+    // }
+    // else{
+    //     socket.emit('responseToAddData',{
+    //         message:"error in adding data"
+    //     })
+    // }
+   }
+
+}
+export default new Todo();
+//here the todo constructor is exported because it will be automatically called after being imported in app.ts file
+
+
+
+
+// const io = returnSocketIo;
 // io.on("connection",(socket)=>{
 //     console.log('user connected-',socket.id);
 //     io.on("disconnect",()=>{
