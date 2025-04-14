f5ca4f334df8c3a1842ffdef14416b027757beb9
Unique Shrestha <uniquestha619@gmail.com>
2025-04-14 15:05:38 +0545

    fixed import paths for deploy

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import express from 'express';
 import path from 'path';
 const app = express();
 
-// import '../todo/todoController';
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
+// Set views directory and templating engine (example: EJS)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); // or 'pug', 'hbs' depending on your template engine
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/',(req,res)=>{
     res.render('home');
 })
